fbd360f0c712d05417f413beb93f97e7a70f4e92
= <mukhtarrahimi.dev@gmail.com>
2025-11-10 21:37:41 +0430

    todo.js schema

---
+++ server/model/Todo.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    is_done: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Task', TaskSchema);
