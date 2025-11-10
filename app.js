1f46045c081e94a5d36d2afc27ffd263029be63a
= <mukhtarrahimi.dev@gmail.com>
2025-11-10 16:17:35 +0430

    update

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+connectDB();
 // Template engine setup
 app.use(expressLayouts);
 app.set('layout', './layouts/main');
 
 app.set('view engine', 'ejs');
 
-connectDB();
+app.use('/', require('./server/routes/main'));
 // server listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
