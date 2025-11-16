985e9bae2115e9245d3564b025cacc768563e08f
= <mukhtarrahimi.dev@gmail.com>
2025-11-16 16:13:37 +0430

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 require('dotenv').config();
+const connectDB = require('./server/config/db');
 
 // public
 app.use(express.static('public'));
 
-// ejs setup
-app.use('engin ejs');
-app.use('layouts main');
+// Template engine setup
+app.use(expressLayouts);
+app.set('layout', './layouts/main');
 
+app.set('view engine', 'ejs');
+
+connectDB();
+// server listen
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server listen to port ${PORT}`);
